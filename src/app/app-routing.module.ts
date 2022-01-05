917e4cf200a529fe86381fe3f2c2de058e9d356e
Kamini-C <iamkaminichhabra@gmail.com>
2022-01-05 10:00:06 +0530

    Changes in Index and About Page
    Created index and About Page,
    Applied Routing,

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ServicesComponent } from './mainContent/services/services.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path : '', redirectTo :'home' , pathMatch : 'full'},
+  {path : 'home', component : MainContentComponent },
+  {path : 'about', component : AboutComponent },
+  {path : '**', component : ErrorpageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './layout/footer/footer.component';
+import { WelcomeBannerComponent } from './mainContent/welcome-banner/welcome-banner.component';
+import { ServicesComponent } from './mainContent/services/services.component';
+import { TestimonialComponent } from './mainContent/testimonial/testimonial.component';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { AboutComponent } from './about/about.component';
+import { AboutIntroComponent } from './about/about-intro/about-intro.component';
+import { AboutWidgetComponent } from './about/about-widget/about-widget.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    WelcomeBannerComponent,
+    ServicesComponent,
+    TestimonialComponent,
+    ErrorpageComponent,
+    MainContentComponent,
+    AboutComponent,
+    AboutIntroComponent,
+    AboutWidgetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
